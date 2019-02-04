e54205bef334395db98c93be798a7b4d90373a6e
Brian Blaylock <u0553130@ad.utah.edu>
2019-02-04 08:10:34 -0700

    updates

---
+++ BB_HRRR/plot_HRRR_figs.py
@@ -0,0 +1,11 @@
+# Brian Blaylock
+# February 1, 2019
+
+"""
+Plot HRRR figures from Pando
+"""
+
+import matplotlib.pyplot as plt 
+
+import sys
+sys.path.
---
+++ JFSP/fires_list.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+def get_fire(fire):
+
+    fires = {'Lake Christine':{'name':'Lake Christine',
+                               'cause':'Human',
+                               'sDATE':datetime(2018,7,6),
+                               'latitude':39.3777,
+                               'longitude':-107.0427},
+             'Whitten':{'name':'Whitten',
+                        'cause':'Undetermined',
+                        'sDATE':datetime(2017, 7, 10),
+                        'latitude':45.36389,
+                        'longitude':-106.6575},
+             'Coal Hollow':{'name':'Coal Hollow',
+                            'cause':'Lightning',
+                            'sDATE':datetime(2018, 8, 4),
+                            'latitude':39.951,
+                            'longitude':-111.403},
+             'Dollar Ridge':{'name':'Dollar Ridge',
+                             'cause':'Human',
+                             'sDATE':datetime(2018,7,11),
+                             'latitude':40.1,
+                             'longitude':-110.96,},
+             'July Storm':{'name':'July Storm',
+                           'cause':'Human',
+                           'sDATE':datetime(2018,7,16),
+                           'latitude':40.77069,
+                           'longitude':-111.96503,},
+             'Cougar Creek':{'name':'Cougar Creek',
+                             'cause':'Lightning',
+                             'sDATE':datetime(2018,9,7),
+                             'latitude':47.851,
+                             'longitude':-120.549,},
+              'Mallard':{'name':'Mallard',
+                                'cause':'Lightning',
+                                'sDATE':datetime(2018,5,14),
+                                'latitude':34.81,
+                                'longitude':-101.306,},
+             }
+
+    return fires[fire]
