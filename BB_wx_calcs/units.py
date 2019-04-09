a3f4d3ed2baefc0b8a67ec0e68e454ee8a63028d
Brian Blaylock <u0553130@ad.utah.edu>
2019-04-09 10:44:20 -0600

    update

--- BB_wx_calcs/units.py
+++ BB_wx_calcs/units.py
@@ -2,52 +2,34 @@
 ## April 9, 2019
 
 # --- Temperature -------------------------------------------------------------
-def K_to_C(T_K):
-    """
-    Convert temperature in Kelvin and return temperature in Celsius
-    Input:
-        T_K - Temperature in Kelvin
-    """
-    return T_K - 273.15
-
-def C_to_K(T_C):
-    """
-    Converts celsius to Kelvin
-    input: 
-         temperature in celsius
-    return:
-         temperature in Kelvin
-    """
-    return T_C + 273.15
+def K_to_C(K):
+    """Convert Kelvin to Celsius"""
+    return K - 273.15
 
 def K_to_F(K):
-    """
-    convert Kelvin to Fahrenheit
-    """
+    """convert Kelvin to Fahrenheit"""
     return (K-273.15)*9/5.+32
-    
+
+def C_to_K(T_C):
+    """Converts celsius to Kelvin"""
+    return T_C + 273.15
+
 def C_to_F(C):
-    """
-    converts Celsius to Fahrenheit
-    """
+    """Converts Celsius to Fahrenheit"""
     return C*9/5.+32
 
-def K_to_C(K):
-    """
-    convert Kelvin to Celsius
-    """
-    return (K-273.15)
+def F_to_C(F):
+    """Convert Fahrenheit to Celsius"""
+    return (F-32) * 5/9
+
 
 # --- Wind --------------------------------------------------------------------
 def mps_to_MPH(mps):
-    """
-    Convert m/s to miles per hour, MPH
-    """
+    """Convert m/s to MPH"""
     return mps * 2.2369
+
 
 # --- Precipitation -----------------------------------------------------------
 def mm_to_inches(mm):
-    """
-    Convert mm to inches
-    """
+    """Convert mm to inches"""
     return mm * 0.0394
